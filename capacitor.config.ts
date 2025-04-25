216dbb0051b6c5492eab0b6dc36e6edc8591ed62
HAI\zhang <1013244149@qq.com>
2025-04-25 09:45:55 +0800

    test

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ng19',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CodePush: {
+      IOS_DEPLOY_KEY: "0wkWluevWItPxkw4QgNemjYgwCze4ksvOXqog",
+      ANDROID_DEPLOY_KEY: "bcb84e5d-20a9-11f0-ac32-0242ac180005",
+      SERVER_URL: "https://server.recodepush.com/"
+    },
+  },
 };
 
 export default config;
